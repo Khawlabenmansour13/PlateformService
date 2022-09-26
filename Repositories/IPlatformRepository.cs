fb167c26e2ddd8ef68019e945dbe7730a58da6af
Khawlabenmansour13 <khawla.benmansour@esprit.tn>
2022-09-26 15:16:38 +0100

    ConnexionWithDB

---
+++ Repositories/IPlatformRepository.cs
@@ -0,0 +1,13 @@
+﻿using PlateformService.Models;
+
+namespace PlateformService.Repositories
+{
+    public interface IPlatformRepository
+    {
+        bool SaveChanges();
+        IEnumerable<Plateform> GetAllPlatforms();
+        Plateform GetPlatFormById(int id);
+        void CreatePlatform(Plateform Plat);
+
+      }  
+}

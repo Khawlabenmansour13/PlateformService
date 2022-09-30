a1193cc64eef091f8f1e03fd69d1d1d7187dd5a0
Khawlabenmansour13 <khawla.benmansour@esprit.tn>
2022-09-30 15:18:09 +0100

    context-seed

---
+++ Dtos/PlateformCreateDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlateformService.Dtos
+{
+    public class PlateformCreateDto
+    {
+        
+       
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Publisher { get; set; }
+        [Required]
+        public string Cost { get; set; }
+
+    }
+}

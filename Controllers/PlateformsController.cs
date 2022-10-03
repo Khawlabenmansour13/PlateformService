130805d5a26627f516c6f50743751331824c19c3
Khawlabenmansour13 <khawla.benmansour@esprit.tn>
2022-10-03 12:00:13 +0100

    get-getall

---
+++ Controllers/PlateformsController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PlateformService.Dtos;
+using PlateformService.Repositories;
+
+namespace PlateformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlateformsController : ControllerBase
+    {
+        private IPlatformRepository _repository;
+        private IMapper _mapper;
+
+        public PlateformsController(IPlatformRepository repository , IMapper mapper) 
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<PlateformReadDto>> GetPlatforms()
+        {
+
+            Console.WriteLine("-->  Getting Platforms.....");
+            var  plateformItem = _repository.GetAllPlatforms();
+            return Ok(_mapper.Map<IEnumerable<PlateformReadDto>>(plateformItem));
+        }
+        [HttpGet("{id}",Name = "GetPlatformById")]
+
+        public ActionResult<PlateformReadDto> GetPlatformById(int id)
+        {
+            var plateformItem = _repository.GetPlatFormById(id);
+            if (plateformItem != null)
+            {
+                return Ok(_mapper.Map<PlateformReadDto>(plateformItem));
+
+
+            }
+            return NotFound();
+
+        }
+
+
+
+
+    }
+}
---
+++ Profiles/PlatformsProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using PlateformService.Dtos;
+using PlateformService.Models;
+
+namespace PlateformService.Profiles
+{
+    public class PlatformsProfile : Profile
+    {
+       public PlatformsProfile() 
+        {
+            CreateMap<Plateform, PlateformReadDto>();
+            CreateMap<PlateformCreateDto, Plateform>();
+        }
+    }
+}
